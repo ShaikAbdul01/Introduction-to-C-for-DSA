ffe734a209f9be8869d5c613e6f139380a4a400c
ShaikAbdul01 <shaikabdulahad2001@gmail.com>
2024-03-12 12:10:04 +0600

    dynamic memory allocation

---
+++ return_array_fun.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int *fun()
+{
+    // int ar[5];
+    int *ar = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> ar[i];
+    }
+    return ar;
+}
+int main()
+{
+    int *ar = fun();
+    for (int i = 0; i < 5; i++)
+    {
+        cout << ar[i] << " ";
+    }
+    return 0;
+}
